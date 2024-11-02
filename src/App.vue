1b05f44c94d1c40715b56cd987a6f58b447fa755
Helton Mori <heltonmori@gmail.com>
2024-11-02 13:07:10 -0300

    Aula 6 atualizando dados com mutations

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,17 @@
     <p v-if="loading">Loading...</p>
     <p v-else-if="error">Something went wrong! Please try again</p>
     <template v-else>
+      <p v-if="activeBook">
+        Update {{ activeBook.title }} rating:
+        <EditRating
+          :initial-rating="activeBook.rating"
+          :book-id="activeBook.id"
+          @closeForm="activeBook = null"
+        />
+      </p>
       <p v-for="book in books" :key="book.id">
-        {{ book.title }}
+        {{ book.title }} = {{ book.rating }}
+        <button @click="activeBook = book">Edit rating</button>
       </p>
     </template>
   </div>
@@ -15,11 +24,16 @@
 import { computed, ref } from "vue";
 import { useQuery } from "@vue/apollo-composable";
 import ALL_BOOKS_QUERY from "../graphql/allBooks.query.gql";
+import EditRating from "./components/EditRating.vue";
 
 export default {
   name: "App",
+  components: {
+    EditRating,
+  },
   setup() {
     const searchTerm = ref("");
+    const activeBook = ref(null);
     const { result, loading, error } = useQuery(
       ALL_BOOKS_QUERY,
       () => ({
@@ -33,7 +47,7 @@ export default {
 
     const books = computed(() => result.value?.allBooks ?? []);
 
-    return { result, searchTerm, loading, error, books };
+    return { result, searchTerm, loading, error, books, activeBook };
   },
 };
 </script>
